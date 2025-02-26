e1a74091bb105524dd05702e00a5b3b48e58a00b
Anthony <a.half915@mybvc.ca>
2025-02-26 08:18:52 -0700

    here it is

---
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -0,0 +1,163 @@
+package com.example.artspace
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.artspace.ui.theme.ArtSpaceTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ArtSpaceTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ArtSpaceLayout()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ImageAndDescription(Picture: Int, PicTitle: String, Photographer: String, modifier: Modifier = Modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Image(
+            painter = painterResource(id = Picture),
+            contentDescription = null,
+            modifier = Modifier
+                .padding(20.dp)
+                .height(300.dp)
+                .width(300.dp)
+                .border(border = BorderStroke(2.dp, Color.Gray), shape = RectangleShape)
+                .shadow(elevation = 4.dp)
+                .padding(10.dp)
+        )
+        Card(
+            modifier = Modifier
+                .padding(horizontal = 45.dp, vertical = 90.dp)
+        ) {
+            Column(
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Text(
+                    text = PicTitle,
+                    fontSize = 25.sp,
+                    textAlign = TextAlign.Start,
+                    lineHeight = 30.sp
+                )
+                Text(
+                    text = Photographer,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = modifier.padding(top = 8.dp)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun ArtSpaceLayout() {
+    var result by remember { mutableStateOf(0) }
+
+    when(result) {
+        1->ImageAndDescription(
+            Picture = R.drawable.image1,
+            PicTitle = stringResource(R.string.lil_elf),
+            Photographer = stringResource(id = R.string.photographer1)
+        )
+        2->ImageAndDescription(
+            Picture = R.drawable.image2,
+            PicTitle = stringResource(R.string.for_miles),
+            Photographer = stringResource(id = R.string.photographer2)
+        )
+        3->ImageAndDescription(
+            Picture = R.drawable.image3,
+            PicTitle = stringResource(R.string.night_city),
+            Photographer = stringResource(id = R.string.photographer3)
+        )
+        else->ImageAndDescription(
+            Picture = R.drawable.image1,
+            PicTitle = stringResource(R.string.lil_elf),
+            Photographer = stringResource(id = R.string.photographer1)
+        )
+    }
+    Row(
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 700.dp)
+    ) {
+        Button(
+            onClick = { result-- },
+            shape = RoundedCornerShape(50.dp),
+            border = BorderStroke(2.dp, Color.Gray),
+            elevation = ButtonDefaults.buttonElevation(
+                defaultElevation = 20.dp
+            )
+        ) {
+            Text(text = "Back")
+        }
+        Button(
+            onClick = { result++ },
+            shape = RoundedCornerShape(50.dp),
+            border = BorderStroke(2.dp, Color.Gray),
+            elevation = ButtonDefaults.buttonElevation(
+                defaultElevation = 20.dp
+            )
+        ) {
+            Text(text = "Next")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ArtSpaceLayoutPreview() {
+    ArtSpaceTheme {
+        ArtSpaceLayout()
+    }
+}
